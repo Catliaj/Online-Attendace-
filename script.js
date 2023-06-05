1e74732913d7f6bb8de14b6d41287a6ec414af3c
Catliaj <124154367+Catliaj@users.noreply.github.com>
2023-06-06 01:08:49 +0800

    script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function authenticate(event) {
     // Check if the username and password are correct
     if (username === 'ict-sevilla 2022-2023' && password === '2022-2023') {
       // Redirect the user to the website
-      window.location.href = 'home.html';
+      window.location.href = 'https://github.com/Catliaj/Online-Attendace-/blob/4dd123700590407d2d0452954c133da7afbda5db/home.html';
     } else {
       // Display an error message
       alert('Invalid username or password');
